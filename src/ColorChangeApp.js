f1eacd05d8fdd9c575e6ae888decb96e80d101a3
SolMarina <sparraa17@gmail.com>
2021-03-16 17:48:25 -0300

    cambio el estado

--- src/ColorChangeApp.js
+++ src/ColorChangeApp.js
@@ -1,27 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ColorChangeApp.css'
 import ChangeBar from './components/changeBar/ChangeBar';
 import ColorfulPicure from './components/colorfulPicture/ColorfulPicture';
 
-const ColorChangeApp = () => {
+const ColorChangeApp = (props) => {
+    const [background, setBackground] = useState('')
+
     return (
         <div className='mainBox'>
             <h2>ColorChangeApp</h2>
             <hr></hr>
-            <div>
-                <div className='changeBar'>
-                    <ChangeBar />
-                </div>
+            <div className='changeBar'>
+                <ChangeBar setBackground={setBackground}/>
             </div>
             <div className='picture'>
-                <ColorfulPicure />
-
+                <ColorfulPicure background= {background}/>
             </div>
-
         </div>
     )
-
-
+ 
 }
 
 export default ColorChangeApp
