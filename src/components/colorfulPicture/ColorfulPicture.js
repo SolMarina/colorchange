83aebfc2a8a218a584c3a9a47c8a4a8264abf32a
SolMarina <sparraa17@gmail.com>
2021-03-18 20:33:16 -0300

    se multiplicaron los cuadrados

--- src/components/colorfulPicture/ColorfulPicture.js
+++ src/components/colorfulPicture/ColorfulPicture.js
@@ -1,28 +1,35 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../colorfulPicture/ColorfulPicture.css';
 
 const ColorfulPicure = (props) => {
     const [backgroundColor, setBackgroundColor] = useState('');
 
-const upcolor= () => {
-    setBackgroundColor(props.background)
-}
-useEffect(() => {
-    upcolor();
-  });
+    const upcolor = () => {
+        setBackgroundColor(props.background)
+    }
+    useEffect(() => {
+        upcolor();
+    });
+    let cuadrado = 5;
+    let list = [];
+    for (let i = 0; i <= cuadrado; i++) {
+        list.push(<div className={backgroundColor === 'azul' ? 'blue'
+            : backgroundColor === 'rojo' ? 'red'
+                : backgroundColor === 'verde' ? 'green' : 'card'}>
+        </div>)
+    }
 
     return (
-
-       
-    <div className={backgroundColor =='azul' ? 'blue'
-    : backgroundColor=='rojo' ? 'red' 
-    : backgroundColor=='verde' ? 'green' : 'card'
-    
-    }>
-       
-    </div>
-        
+        list
     )
+
+
+
+
+
+
+
+
 }
 export default ColorfulPicure;
 
